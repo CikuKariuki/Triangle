b2794181ca5912058c365feaf44896385084ebd4
Ciku Kariuki <wanjikukariuki03@gmail.com>
2019-02-17 19:31:31 +0300

    set conditions in js

--- triangle.js
+++ triangle.js
@@ -3,4 +3,14 @@ function tracker() {
   var sideB=parseInt(document.getElementById("sideB").value);
   var sideC=parseInt(document.getElementById("sideC").value);
   var side = [sideA, sideB, sideC];
-  
+  if (side[0] + side[1] <= side[2] || side[0] + side[2]<=side[1] || side[1] + side[2] <= side[0]){
+    alert("That is not a triangle!");
+  }else if(side[0] === side[1] && side[2] === side[1] && side[0] === side[2]){
+    alert("That's an equilateral triangle");
+  }else if (side[0]===side[1] || side[1]===side[2] || side[2]===side[0]){
+    alert("That's an isosceles triangle");
+  }
+  else{
+    alert("That's a scalene triangle");
+  }
+}
