792605db77fda8fa5d01b1ef5865abb054fcf445
Ciku Kariuki <wanjikukariuki03@gmail.com>
2019-02-17 19:31:03 +0300

    set variables in js

--- triangle.js
+++ triangle.js
@@ -1,1 +1,6 @@
 function tracker() {
+  var sideA=parseInt(document.getElementById("sideA").value);
+  var sideB=parseInt(document.getElementById("sideB").value);
+  var sideC=parseInt(document.getElementById("sideC").value);
+  var side = [sideA, sideB, sideC];
+  
