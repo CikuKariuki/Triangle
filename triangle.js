e36ba04018a43ef5e54d3e0a0dc63213767fa4c8
Wanjiku <Wanjiku>
2019-10-31 14:32:50 +0300

    sorted scalene issue

--- triangle.js
+++ triangle.js
@@ -9,7 +9,7 @@ function tracker() {
     document.getElementById("display").innerHTML="That's an equilateral triangle";
   }else if (side[0]===side[1] || side[1]===side[2] || side[2]===side[0]){
   document.getElementById("display").innerHTML="That's an isosceles triangle"
-  }else if(side[0]=== blank || side[1] === blank || side[2] === blank){
+  }else if(side[0].value || side[1].value || side[2].value){
     document.getElementById("display").innerHTML="Please put in some numbers";
   }else {
     document.getElementById("display").innerHTML="That is a scalene triangle";
